08f02ec9cf7c5ba5a466359c6f87c3010492cc21
tekunosadunesu <133689768+tekunosadunesu@users.noreply.github.com>
2023-05-16 13:56:42 +0200

    laby 10 - sortowanie babelkowe, animacja sortowania

--- 10_bubble_sort.py
+++ 10_bubble_sort.py
@@ -7,14 +7,15 @@ liczby = [5, 1, 100, 3, 6, 2, 4, 15, 67, 24, 5, 0, 12, 54, 66, 0, 7]
 
 
 def bubble_sort(lista):
-    licznik = 5
-    while licznik != 0:
-        licznik = 0
+    licznik = 5  # losowa liczba, po to zeby weszlo do petli
+    while licznik != 0:  # wykonuje sie dopoki licznik(ile razy sie zmienilo miejscami) nie jest 0 - jest posortowane
+        licznik = 0 
         for j in range(len(lista) - 1):
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
                 licznik += 1
-
+                
+                # animacja sortowania
                 plt.clf()
                 plt.bar(range(len(lista)), lista)
                 display.display(plt.gcf())
