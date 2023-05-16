e7d2fced84db41fd4c9435176c15b1b9f4304fa7
tekunosadunesu <133689768+tekunosadunesu@users.noreply.github.com>
2023-05-16 13:59:05 +0200

    laby 9 - merge sort

--- 9_merge_sort.py
+++ 9_merge_sort.py
@@ -12,17 +12,19 @@ print("Lista przed sortowaniem:", liczby)
 
 def merge_sort(lista):
 
-    if len(lista) > 1:
-        polowa = int(len(lista) // 2)
+    # dzielenie listy na pol
+    if len(lista) > 1: 
+        polowa = int(len(lista) // 2) 
         print(lista)
         prawa_lista = lista[polowa :]
         lewa_lista = lista[: polowa]
         print(lewa_lista, prawa_lista)
-        merge_sort(lewa_lista)
+        merge_sort(lewa_lista)  # rekurencja
         merge_sort(prawa_lista)
-
+        
+        # wstawianie wartosci do koncowej listy
         i = j = k = 0
-        while i < len(lewa_lista) and j < len(prawa_lista):
+        while i < len(lewa_lista) and j < len(prawa_lista): 
             if lewa_lista[i] < prawa_lista[j]:
                 lista[k] = lewa_lista[i]
                 i += 1
